358747aa17409f5d5ab3d184e514ef252fd38cf5
Eman Naveed <emannaveed127@gmail.com>
2024-10-10 09:54:06 +0500

    signup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #incluude <iostream>
+#incluude <signup.cpp>
 using namespace std;
 int main()
 {
 cout<<"hello world"<<endl;
 cout<<"second line of the main the code"<<endl;
+signup()
 return 0;
 
 
